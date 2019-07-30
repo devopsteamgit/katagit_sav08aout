0a215c4e1e7e2f8bb6d65c14337ccc9de20d53e3
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-07-30 15:14:04 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -3,7 +3,7 @@ docker network create mynetwork;
 docker pull 22072017/ubuntu:16.04;
 docker run -dt --name git_remote  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;
 docker run -dt --name dev1_local  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;
-docker run -dt --name dev1_local  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;
+docker run -dt --name dev2_local  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;
 docker exec git_remote bash -c "apt-get update && chmod 777 /work_dir/training-files";
 docker exec dev1_local bash -c "apt-get update && chmod 777 /work_dir/training-files";
 docker exec dev2_local bash -c "apt-get update && chmod 777 /work_dir/training-files";

3d297dd69de405cac98af64d259cfc4f5f7a54dc
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-07-30 18:22:00 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -1,6 +1,6 @@
 ssh host01;
 docker network create mynetwork;
-docker pull 22072017/ubuntu:16.04;
+docker pull testgitessai/ubuntu:18.04;
 docker run -dt --name git_remote  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;
 docker run -dt --name dev1_local  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;
 docker run -dt --name dev2_local  --network mynetwork -v /root:/work_dir/training-files 22072017/ubuntu:16.04 /bin/bash;

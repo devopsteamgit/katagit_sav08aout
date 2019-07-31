a8bf667fa699c2a77a48f57f30e600d443ee626e
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-07-31 18:05:40 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -4,9 +4,9 @@ docker pull testgitessai/ubuntu:18.04;
 docker run -dt -p 0.0.0.0:2255:22 --name git_remote  --network mynetwork -v /root:/work_dir/training-files testgitessai/ubuntu:18.04 /bin/bash;
 docker run -dt -p 0.0.0.0:2256:22 --name dev1_local  --network mynetwork -v /root:/work_dir/training-files testgitessai/ubuntu:18.04 /bin/bash;
 docker run -dt -p 0.0.0.0:2257:22 --name dev2_local  --network mynetwork -v /root:/work_dir/training-files testgitessai/ubuntu:18.04 /bin/bash;
-docker exec git_remote bash -c "apt-get update && chmod 777 /work_dir/training-files";
-docker exec dev1_local bash -c "apt-get update && chmod 777 /work_dir/training-files";
-docker exec dev2_local bash -c "apt-get update && chmod 777 /work_dir/training-files";
+docker exec git_remote bash -c "apt-get update && chmod 777 /work_dir/training-files;service ssh restart";
+docker exec dev1_local bash -c "apt-get update && chmod 777 /work_dir/training-files;service ssh restart";
+docker exec dev2_local bash -c "apt-get update && chmod 777 /work_dir/training-files;service ssh restart";
 
 docker inspect git_remote  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v \"\"  | awk -F ":" '{print $2" git_remote"}'  | sed  -e "s/\"//g" | sed -e "s/,//g" >/tmp/tempo_hosts 
 docker inspect dev1_local  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v \"\"  | awk -F ":" '{print $2" dev1_local"}'  | sed  -e "s/\"//g" | sed -e "s/,//g" >>/tmp/tempo_hosts

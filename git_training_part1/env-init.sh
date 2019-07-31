a60389597dd9732fc0983c92f3d04a9a42532f3b
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-07-31 17:35:27 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -7,3 +7,14 @@ docker run -dt -p 0.0.0.0:2257:22 --name dev2_local  --network mynetwork -v /roo
 docker exec git_remote bash -c "apt-get update && chmod 777 /work_dir/training-files";
 docker exec dev1_local bash -c "apt-get update && chmod 777 /work_dir/training-files";
 docker exec dev2_local bash -c "apt-get update && chmod 777 /work_dir/training-files";
+
+docker inspect git_remote  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v \"\"  | awk -F ":" '{print $2" git_remote"}'  | sed  -e "s/\"//g" | sed -e "s/,//g" >/tmp/tempo_hosts 
+docker inspect dev1_local  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v \"\"  | awk -F ":" '{print $2" dev1_local"}'  | sed  -e "s/\"//g" | sed -e "s/,//g" >>/tmp/tempo_hosts
+docker inspect dev2_local  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v \"\"  | awk -F ":" '{print $2" dev2_local"}'  | sed  -e "s/\"//g" | sed -e "s/,//g" >>/tmp/tempo_hosts
+docker cp /tmp/tempo_hosts git_remote:/tmp/tempo_hosts
+docker cp /tmp/tempo_hosts dev1_local:/tmp/tempo_hosts
+docker cp /tmp/tempo_hosts dev2_local:/tmp/tempo_hosts
+
+docker exec -it  git_remote bash -c "cat /tmp/tempo_hosts >>/etc/hosts"
+docker exec -it  dev1_local bash -c "cat /tmp/tempo_hosts >>/etc/hosts"
+docker exec -it  dev2_local bash -c "cat /tmp/tempo_hosts >>/etc/hosts"

30521ea303bdf6ce25090db33bfd8c6fd7e94408
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-08-01 19:29:52 +0200

    Update env-init.sh

--- git_training_part2/env-init.sh
+++ git_training_part2/env-init.sh
@@ -22,4 +22,4 @@ docker exec -it  dev2_local bash -c "cat /tmp/tempo_hosts >>/etc/hosts"
 # creation remote repository 
 # docker exec git_remote bash -c "su - git -c \"mkdir formteam;cd formteam;git init --bare\"";
 # création repo local connecte au remote
-docker exec dev1_gitlocal bash -c "su - git -c \"git init;git config --global user.email \"git@example.com\";git config --global user.name \"git Name\";pwd;git config --list;git remote add origin ssh://git@git_remote/home/git/formteam\"";
+docker exec dev1_local bash -c "su - git -c \"git init;git config --global user.email \"git@example.com\";git config --global user.name \"git Name\";pwd;git config --list;git remote add origin ssh://git@git_remote/home/git/formteam\"";

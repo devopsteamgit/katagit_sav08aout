56d560677b627d96afbc05a3dfd38ea2c736bb88
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-08-02 18:38:48 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -2,9 +2,3 @@ ssh host01;
 docker network create mynetwork;
 docker pull testgitessai/ubuntu:18.04;
 docker run -dt -p 0.0.0.0:2256:22 --name dev1_git_local  --network mynetwork -v /root:/work_dir/training-files testgitessai/ubuntu:18.04 /bin/bash;
-docker exec dev1_git_local bash -c "apt-get update && chmod 777 /work_dir/training-files;service ssh restart";
-
-docker inspect dev1_git_local  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v \"\"  | awk -F ":" '{print $2" dev1_git_local"}'  | sed  -e "s/\"//g" | sed -e "s/,//g" >>/tmp/tempo_hosts
-docker cp /tmp/tempo_hosts dev1_git_local:/tmp/tempo_hosts
-
-docker exec -it  dev1_git_local bash -c "cat /tmp/tempo_hosts >>/etc/hosts"

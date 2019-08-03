d3ef6e5bbc72bfdb6fefecbdf44b980804e46e8b
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-08-03 21:24:26 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -3,4 +3,9 @@ docker network create mynetwork;
 docker pull testgitessai/ubuntu:18.04;
 docker run -dt -p 0.0.0.0:2256:22 --name dev1_git_local  --network mynetwork -v /root:/work_dir/training-files testgitessai/ubuntu:18.04 /bin/bash;
 
+docker exec  dev1_git_local bash -c "su - git -c \"mkdir repo;cd repo;git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name\"";
+docker exec  dev1_git_local bash -c "su - git -c \"cd repo;git add --all ;git commit -a -m 'creation branch master';mkdir repo/PROJET\"";
+
+docker exec  dev1_git_local bash -c "su - git -c \"mkdir repofiles;cd repofiles;git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name\"";
+docker exec  dev1_git_local bash -c "su - git -c \"cd repofiles;git remote add origin https://github.com/testgitpub/testfiles.git;git pull origin master\"";
 https://github.com/testgitpub/testfiles.git

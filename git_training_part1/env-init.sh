0090b173040c4443dcce45bea3cbe142d4a0c404
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-08-03 21:38:08 +0200

    Update env-init.sh

--- git_training_part1/env-init.sh
+++ git_training_part1/env-init.sh
@@ -7,5 +7,5 @@ docker exec  dev1_git_local bash -c "su - git -c \"mkdir repofiles;cd repofiles;
 docker exec  dev1_git_local bash -c "su - git -c \"cd repofiles;git remote add origin https://github.com/testgitpub/testfiles.git;git pull origin master\"";
 
 docker exec  dev1_git_local bash -c "su - git -c \"mkdir PROJET;cd PROJET;git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name\"";
-docker exec  dev1_git_local bash -c "su - git -c \"cd PROJET;cp ../repofiles/script_1.sh .;git add --all ;git commit -a -m 'ajout script_1.sh'\"";
-docker exec  dev1_git_local bash -c "su - git -c \"cd PROJET;cp ../repofiles/script_2.sh .;git add --all ;git commit -a -m 'ajout script_2.sh'\"";
+docker exec  dev1_git_local bash -c "su - git -c \"cd PROJET;cp ../repofiles/filess/script_1.sh .;git add --all ;git commit -a -m 'ajout script_1.sh'\"";
+docker exec  dev1_git_local bash -c "su - git -c \"cd PROJET;cp ../repofiles/files/script_2.sh .;git add --all ;git commit -a -m 'ajout script_2.sh'\"";

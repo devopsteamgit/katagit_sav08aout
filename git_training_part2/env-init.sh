d49765fc3305c0990fdeb7973bb691052ad62031
testgitessai <53436956+testgitessai@users.noreply.github.com>
2019-08-05 15:21:04 +0200

    Update env-init.sh

--- git_training_part2/env-init.sh
+++ git_training_part2/env-init.sh
@@ -7,19 +7,9 @@ docker inspect dev1_git_local  | grep  \"IPAddress\" | sed -e "s/ //g" | grep -v
 docker cp /tmp/tempo_hosts dev1_git_local:/tmp/tempo_hosts
 docker exec dev1_git_local bash -c "cat /tmp/tempo_hosts >>/etc/hosts"
 
-#docker exec  dev1_git_local bash -c "su - git -c \"git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name\"";
-#docker exec  dev1_git_local bash -c "su - git -c \"git add --all ;git commit -a -m 'creation branch master';mkdir PROJET\"";
-
-#docker exec dev1_git_local bash -c "su - git -c \"git init;git config --global user.email \"git@example.com\";git config --global user.name \"git Name\";git add --all ;git commit -a -m \"creation branch master \";mkdir PROJET\"";
-#docker exec dev1_git_local bash -c "su - git -c \"echo \"printf 'Ceci est un script test\n\'\" > script_1.sh;cat script_1.sh\";git add script_1.sh;git commit -m 'ajout du script_1.sh dans le repo '";
-
 docker exec  dev1_git_local bash -c "mkdir /home/repofiles;cd /home/repofiles;git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name";
 docker exec  dev1_git_local bash -c "cd /home/repofiles;git remote add origin https://github.com/testgitpub/testfiles.git;git pull origin master";
 
-#docker exec  dev1_git_local bash -c "su - git -c \"cp /home/repofiles/files/cdfic .\"";
-
-
-#docker exec  dev1_git_local bash -c "su - git -c \"mkdir PROJET;cd PROJET;git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name\"";
 docker exec  dev1_git_local bash -c "su - git -c \"git init;git config --list; git config --global user.email \"git@example.com\";git config --global user.name  Name\"";
 docker exec  dev1_git_local bash -c "su - git -c \"cp /home/repofiles/files/script_1.sh .;git add --all ;git commit -a -m 'ajout script_1.sh'\"";
 docker exec  dev1_git_local bash -c "su - git -c \"cp /home/repofiles/files/script_2.sh .;git add --all ;git commit -a -m 'ajout script_2.sh'\"";
